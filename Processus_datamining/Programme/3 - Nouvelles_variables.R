8d91584c3bdd128f4ad1e3df62b2cf00baea3f5c
Rachid Sahli <134428938+rachidsahli@users.noreply.github.com>
2025-01-02 16:12:54 +0100

    Add files via upload

---
+++ Processus_datamining/Programme/3 - Nouvelles_variables.R
@@ -0,0 +1,9 @@
+#_______________________________________________________________________________
+# COURS     : SCORING
+# ANNEE     : 2025
+# AUTEUR    : Rachid SAHLI
+#
+# DONNEES   : Agence de voyage
+#
+# PROGRAMME : 3 - Création de nouvelles variables
+#_______________________________________________________________________________
