084c2d611fb40c2a20b547d529078c751ea99ff0
rachidsahli <rachidsahli144@gmail.com>
2025-01-12 23:59:10 +0100

    Initialisation

---
+++ App_finance/Programme/0 - Environnement.R
@@ -0,0 +1,51 @@
+#_______________________________________________________________________________
+# Projet Séries temporelles
+# ANNEE     : 2025
+# AUTEUR    : Rachid SAHLI
+#
+#
+# PROGRAMME : 0 - ENVIRONNEMENT
+#_______________________________________________________________________________
+
+
+# Import library -----
+
+library(tidyverse)      # Manipulation des données et graphiques
+library(conflicted)     # Gestion des conflits de noms de fonctions entre packages
+library(knitr)          # Création de tableau
+library(gridExtra)      # Arrangement de graphique
+library(lubridate)      # Gestion des dates
+library(questionr)      # Statistiques descriptives
+library(skimr)          # Statistiques descriptives
+library(BioStatR)       # Mesures de liaison pour les variables quantitatives (rapport de corrélation)
+library(DescTools)      # Mesures de liaison pour les variables qualitatives (Cramer, Tschuprow)
+library(FactoMineR)     # Caractérisation des classes
+library(naniar)         # Gestion des valeurs manquantes
+library(zoo)            # Gestion des valeurs manquantes
+library(corrplot)       # Corrélation
+library(woeBinning)     # Discrétisation supervisée
+library(smbinning)      # Discrétisation supervisée
+library(caret)          # Modélisation - Général
+library(pROC)           # Modélisation - Général
+library(carData)        # Modélisation - Régression logistique
+library(car)            # Modélisation - Régression logistique
+library(blorr)          # Modélisation - Régression logistique
+library(emmeans)        # Modélisation - Régression logistique
+library(broom)          # Modélisation - Régression logistique
+library(glmnet)         # Modélisation - Régression logistique pénalisée
+library(rpart)          # Modélisation - Arbre de décision
+library(rpart.plot)     # Modélisation - Arbre de décision
+library(class)          # Modélisation - k plus proches voisins
+
+# Gestion des conflits -----
+
+conflict_prefer("select", "dplyr")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("slice", "dplyr")
+
+# Import data -----
+
+cac_40 <- read.table("~/Documents/Projet-datascience/App_finance/Data/cac_40.csv", 
+                        encoding = "UTF-8", sep = ",",
+                        header = TRUE, na.strings = "")
+
