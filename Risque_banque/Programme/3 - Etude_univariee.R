90592d9e642d50f8a1296b564d2a6fe3688302ec
rachidsahli <rachidsahli144@gmail.com>
2025-01-20 02:12:23 +0100

    Intro et Qual

--- Risque_banque/Programme/3 - Etude_univariee.R
+++ Risque_banque/Programme/3 - Etude_univariee.R
@@ -9,10 +9,10 @@
 
 # Variables quantitatives 
 
-var_quanti <- sapply(periode_2021, is.numeric)
-lapply(names(periode_2021)[var_quanti], function(var) plot_quanti(periode_2021, var))
+var_quanti <- sapply(periode_2023, is.numeric)
+lapply(names(periode_2023)[var_quanti], function(var) plot_quanti(periode_2023, var))
 
 # Variables qualitatives
 
-var_quali <- sapply(periode_2021, function(x) is.factor(x) || is.character(x))
-lapply(names(periode_2021)[var_quali], function(var) plot_quali(periode_2021, var))
+var_quali <- sapply(periode_2023, function(x) is.factor(x) || is.character(x))
+lapply(names(periode_2023)[var_quali], function(var) plot_quali(periode_2023, var))
