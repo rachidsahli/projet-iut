d104c02da22d7ddcb21ef28244b2113b7537ae61
rachidsahli <rachidsahli144@gmail.com>
2025-02-14 21:04:27 +0100

    Initialisation

---
+++ Serie_temp_charbon/scripts/0_environnement.R
@@ -0,0 +1,9 @@
+# Import data ----
+
+charbon <- read.table("/Users/rs777/Documents/Projet-datascience/Serie_temp_charbon/charbon_usa.txt", col.names = "Charbon mWh")
+
+# Transfomation en série temp ----
+
+charbon <- ts(charbon, frequency = 12, start = c(2001, 1))
+
+# plot(charbon) # Visualisation
