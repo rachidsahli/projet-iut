d20ac05138d331445bbc67f72f689de1c8005dc0
rachidsahli <rachidsahli144@gmail.com>
2025-02-14 21:15:05 +0100

    Decomposition

---
+++ Serie_temp_charbon/scripts/3_decomposition.R
@@ -0,0 +1,40 @@
+# Objectif : Calculer les CVS et decompositionner la série
+
+source('/Users/rs777/Documents/Projet-datascience/Serie_temp_charbon/scripts/0_environnement.R')
+
+# Décomposition de la série ----
+
+dec = decompose(charbon, type = "additive") # Décomposition de la série
+plot(dec)
+
+plot(dec$random, main="Graphe des résidus",
+     xlab="Année", ylab="résidus")
+abline(0,0)
+abline(2*sd(dec$random, na.rm=TRUE),0)
+abline(-2*sd(dec$random, na.rm=TRUE),0) # Graphe des résidus
+
+
+boxplot(dec$random, na.rm=TRUE,col="yellow",
+        main="Boîte à moustache des résidus" )
+
+
+plot(dec$trend + dec$random, xlab="", main="Série corrigée des variations saisonnières", ylab="", lwd=2)
+
+plot(dec$trend + dec$seasonal,xlab="", lwd=2, main="Série lissée des prédictions", ylab="")
+
+# Comparaison des deux courbes précédentes
+
+plot(charbon, main="", ylab="", xlab="Année")
+lines(dec$trend + dec$seasonal, col="red")
+lines(dec$trend + dec$random, col = "green")
+legend("topright", 
+       legend = c("Données", "Série corrigée des variations saisonnières", "Série lissée des prédictions"),
+       text.col = c("black", "red", "green"))
+
+
+
+
+
+
+
+
